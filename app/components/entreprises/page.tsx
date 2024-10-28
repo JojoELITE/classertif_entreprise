1b595793dec6c16e5df6940a1941fe593a632e64
elieboulingui <elieboulingui2@gmail.com>
2024-10-28 14:38:58 +0100

    i create comment

--- app/components/entreprises/page.tsx
+++ app/components/entreprises/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Heard() {
   return (
     <div className=' h-44 w-full'>
-   <div></div>
+   <div style={{background:"/image/group.jpeg"}}></div>
     </div>
   )
 }
