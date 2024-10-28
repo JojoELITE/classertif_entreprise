dfe56982e560ed2f69ce490a4d81536595828be4
elieboulingui <elieboulingui2@gmail.com>
2024-10-28 15:16:04 +0100

    page

--- app/components/entreprises/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Heard() {
-  return (
-    <div className=' h-44 w-full'>
-   <div style={{background:"/image/group.jpeg"}}></div>
-    </div>
-  )
-}
---
+++ app/pages/DetailsListEntreprise/page.tsx
@@ -0,0 +1,10 @@
+import DetailsListEntrepriseHearder from '@/components/DetailsListEntreprise/DetailsListEntrepriseHearder'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+   <DetailsListEntrepriseHearder/>
+    </div>
+  )
+}
