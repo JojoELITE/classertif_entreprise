2f8898dcb1d74e6d3ce871b0051f11fc05401974
elieboulingui <elieboulingui2@gmail.com>
2024-10-28 15:37:01 +0100

    page i create

--- app/pages/DetailsListEntreprise/page.tsx
+++ app/pages/DetailsListEntreprise/page.tsx
@@ -1,10 +1,11 @@
-import DetailsListEntrepriseHearder from '@/components/DetailsListEntreprise/DetailsListEntrepriseHearder'
+
+import SectionHeroDetails from '@/components/DetailsListEntreprise/SectionHeroDetails'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-   <DetailsListEntrepriseHearder/>
+   <SectionHeroDetails/>
     </div>
   )
 }
