949f232862935138d740910eddfc7360ef839935
elieboulingui <elieboulingui2@gmail.com>
2024-10-29 14:04:39 +0100

    i hidden all overflow

--- components/DetailsListEntreprise/SectionHeroDetails.tsx
+++ components/DetailsListEntreprise/SectionHeroDetails.tsx
@@ -10,14 +10,16 @@ export default function SectionHeroDetails() {
     <div className="flex flex-col gap-9 h-full">
       <div className="relative h-full w-full flex-grow">
         <div className="absolute inset-0">
-          <Image
-            src="/image/image_cover.png"
-            layout="fill"
-            alt="Vue d'une entreprise"
-            className="object-cover object-center"
-          />
+        <Image
+          src="/image/image_cover.png"
+          width={1000}
+          height={0}
+          alt="Vue d'une entreprise"
+          className="h-[300px] w-full object-cover object-center"
+        />
+
         </div>
-        <div className="relative z-10 flex gap-5 items-center pt-20 pl-5 lg:pl-12">
+        <div className="relative z-10 flex gap-5 items-center pt-64 pl-5 lg:pl-12">
           <div className="lg:w-32 w-20 lg:h-24 h-20 bg-white border shadow-lg rounded flex items-center justify-center p-4" style={{ marginBottom: '-30px' }}>
             <span className="font-bold text-lg">Mazars</span>
           </div>
